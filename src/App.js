3f112b6a67b2e89f8286fef50978d10bd4e8d989
Amna Saad <106149828+amna55@users.noreply.github.com>
2025-09-22 17:20:09 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import { Analytics } from "@vercel/analytics/react"
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Header from "./component/header";
 import About from "./component/about";
